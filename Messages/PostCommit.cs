74d758e30524e0029891efb475df2d107d83c5df
anloffredo <loffredo.ap@gmail.com>
2019-09-22 09:40:57 -0700

    Pre-Post commit messages

---
+++ Messages/PostCommit.cs
@@ -0,0 +1,14 @@
+﻿using Penguin.Messaging.Abstractions.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Messaging.Persistence.Messages
+{
+    /// <summary>
+    /// Fires after a persistence commit has completed
+    /// </summary>
+    public class PostCommit : Message
+    {
+    }
+}
---
+++ Messages/PreCommit.cs
@@ -0,0 +1,14 @@
+﻿using Penguin.Messaging.Abstractions.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Messaging.Persistence.Messages
+{
+    /// <summary>
+    /// Fires on the beginning of a persistence commit
+    /// </summary>
+    public class PreCommit : Message
+    {
+    }
+}
