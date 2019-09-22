6a59693d4ad503e98d56dd09eb0b20ab39ef9042
anloffredo <loffredo.ap@gmail.com>
2019-09-22 08:24:31 -0700

    changed property values added to update message

--- Messages/Updated.cs
+++ Messages/Updated.cs
@@ -1,4 +1,6 @@
-﻿namespace Penguin.Messaging.Persistence.Messages
+﻿using System.Collections.Generic;
+
+namespace Penguin.Messaging.Persistence.Messages
 {
     /// <summary>
     /// A post-save update message. Inherits from saved
@@ -7,6 +9,16 @@
     public class Updated<T> : Saved<T>
     {
         #region Constructors
+
+        /// <summary>
+        /// A Dictionary containing the changed property name / values for the new version of the entity
+        /// </summary>
+        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
+
+        /// <summary>
+        /// A Dictionary containing the changed property / values as they were on the old version of the entity
+        /// </summary>
+        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
 
         /// <summary>
         /// Creates a new instance of the message with the object being referenced attached
